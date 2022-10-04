ec597c1946f4890502a24a4800f8305dd738fc89
RuslanUC <dev_ruslan_uc@protonmail.com>
2022-10-04 14:27:50 +0300

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from json import dumps as jdumps
 from json import loads as jloads
 from random import randint, random
 from time import time
+from typing import List
 from urllib.parse import urlparse
 
 from aiohttp import ClientSession
@@ -13,36 +14,54 @@ from selenium.webdriver.chrome.service import Service
 
 from utils import mouse_curve, getUrl
 
-#SITEKEY =
-#URL = "accounts.hcaptcha.com"
-
 class AioHcaptcha:
     def __init__(self, sitekey, url, captcha_callback):
         self.sitekey = sitekey
         self.url = url
         self.domain = urlparse(url).netloc
 
-        self._s = None
+        self._c = None
+        self._start = None
+        self._script = {}
         self._question_callback = captcha_callback
 
     async def _getN(self):
         if self._c["type"] == "hsw":
-            tok = self._c["req"]
-            t = tok.split(".")[1].encode("utf8")
-            t += b'=' * (-len(t) % 4)
-            d = jloads(b64decode(t).decode("utf8"))
-            scr = await getUrl(f"{d['l']}/hsw.js")
-            return await self._solve_hsw(scr, tok)
+            token = self._c["req"].split(".")[1].encode("utf8")
+            token += b'=' * (-len(token) % 4)
+            d = jloads(b64decode(token).decode("utf8"))
+            if f"hsw_{d['l']}" not in self._script:
+                self._script[f"hsw_{d['l']}"] = await getUrl(f"{d['l']}/hsw.js")
+            return await self._solve_hsw(d['l'])
         elif self._c["type"] == "hsl":
             await self._solve_hsl(self._c["req"])
 
-    async def _getMotionData(self, start):
-        j = {
-            "st": start+1000+randint(10, 100),
+    async def _solve_hsw(self, sc):
+        token = self._c["req"]
+        script = self._script[f"hsw_{sc}"]
+        def _hsw():
+            options = ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            options.add_argument('--disable-extensions')
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            driver = Chrome(service=Service(executable_path="chromedriver.exe"), options=options)
+            #with open("test_hsw.js", "w") as f:
+            #    f.write(f"{script}\n\nreturn await hsw(\"{token}\");")
+            return driver.execute_script(f"{script}\n\nreturn await hsw(\"{token}\");")
+        return await get_event_loop().run_in_executor(ThreadPoolExecutor(4), _hsw)
+
+    async def _solve_hsl(self, token):
+        raise RuntimeError("HSL not implemented!")
+
+    @property
+    def _motionData(self):
+        return {
+            "st": self._start+1000+randint(10, 100),
             "v": 1,
             "topLevel": {
                 "inv": False,
-                "st": start,
+                "st": self._start,
                 "sc": {"availWidth": 1920,
                        "availHeight": 1022,
                        "width": 1920,
@@ -133,6 +152,20 @@ class AioHcaptcha:
             }
         }
 
+    def _getMp(self, arr: List[List[int]]) -> float:
+        mp = 0
+        count = 0
+        for idx, item in enumerate(arr):
+            if idx == 0:
+                continue
+            delta = item[2]-arr[idx-1][2]
+            mp = (mp * count + delta) / (count + 1)
+            count += 1
+        return mp
+
+    async def _getMotionData(self):
+        j = self._motionData
+
         curve = mouse_curve(
             (randint(0, 480), randint(0, 270)),
             (randint(1440, 1920), randint(810, 1022)),
@@ -140,10 +173,10 @@ class AioHcaptcha:
         )
         idx = randint(23, 30)
         mm = deepcopy(curve[:idx])
-        mm_mp = randint(10, 20)+random()*randint(1, 4) # TODO: ???
+        mm_mp = randint(10, 20) + random() * randint(1, 4)  # TODO: ???
         md = curve[idx].copy()
         mu = curve[idx].copy()
-        ctime = start
+        ctime = self._start
         for m in mm:
             ctime += randint(20, 40)
             m.append(ctime)
@@ -165,12 +198,12 @@ class AioHcaptcha:
         )
         idx = randint(60, 70)
         mm = curve[:idx]
-        ctime = start+1000+randint(100, 150)
+        ctime = self._start + 1000 + randint(100, 150)
         for m in mm:
             ctime += randint(20, 40)
             m.append(ctime)
         j["topLevel"]["mm"] = mm
-        j["topLevel"]["mm-mp"] = randint(100, 120)+random()*randint(1, 4)
+        j["topLevel"]["mm-mp"] = randint(100, 120) + random() * randint(1, 4)
 
         j["topLevel"]["wn"] = []
         j["topLevel"]["wn-mp"] = 0
@@ -178,24 +211,13 @@ class AioHcaptcha:
         j["topLevel"]["xy-mp"] = 0
         return j
 
-    async def _solve_hsw(self, script, token):
-        def _hsw():
-            options = ChromeOptions()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            options.add_argument('--disable-extensions')
-            options.add_experimental_option('excludeSwitches', ['enable-logging'])
-            driver = Chrome(service=Service(executable_path="chromedriver.exe"), options=options)
-            #with open("test_hsw.js", "w") as f:
-            #    f.write(f"{script}\n\nreturn await hsw(\"{token}\");")
-            return driver.execute_script(f"{script}\n\nreturn await hsw(\"{token}\");")
-        return await get_event_loop().run_in_executor(ThreadPoolExecutor(4), _hsw)
-
-    async def _solve_hsl(self, token):
-        raise RuntimeError("HSL not implemented!")
+    async def _getMotionDataForSolved(self, answers):
+        j = self._motionData
+        j["dct"] = j["st"]
+        return j
 
     async def solve(self):
-        start = int(time() * 1000) - 2000
+        self._start = int(time() * 1000) - 2000
         sess = ClientSession(headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"})
         siteconfig = await sess.post("https://hcaptcha.com/checksiteconfig", params={
@@ -213,7 +235,7 @@ class AioHcaptcha:
             "hl": "en-US",
             "n": await self._getN(),
             "c": jdumps(self._c),
-            "motionData": await self._getMotionData(start)
+            "motionData": await self._getMotionData()
         }, headers={"Content-Type": "application/x-www-form-urlencoded"})
 
         captcha = await captcha.json()
@@ -231,7 +253,7 @@ class AioHcaptcha:
             "c": jdumps(self._c),
             "n": await self._getN(),
             "sitekey": self.sitekey,
-            "motionData": jdumps(await self._getMotionData(start))
+            "motionData": jdumps(await self._getMotionDataForSolved(answers))
         })
 
         r = await res.text()
@@ -262,6 +284,5 @@ async def main():
     print(resp)  # {"success":false,"error-codes":["invalid-data"]} ??
 
 if __name__ == "__main__":
-    from asyncio import run, get_event_loop
-
-    run(main())
+    from asyncio import get_event_loop
+    get_event_loop().run_until_complete(main())
