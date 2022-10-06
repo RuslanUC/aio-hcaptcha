d19d43cd0c8ce06e8019b4c06b3c312365bff560
RuslanUC <dev_ruslan_uc@protonmail.com>
2022-10-06 13:45:44 +0300

    add more headers

--- async_hcaptcha/asynchcaptcha.py
+++ async_hcaptcha/asynchcaptcha.py
@@ -381,7 +381,10 @@ class AioHcaptcha:
 
         self._start = int(time() * 1000 - 2000)
         sess = ClientSession(headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+            "Origin": "https://newassets.hcaptcha.com",
+            "Referer": "https://newassets.hcaptcha.com/",
+            "dnt": "1",
         })
 
         api_js = await sess.get("https://js.hcaptcha.com/1/api.js")
