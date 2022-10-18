9448e8f79ef1d7c49288345a0e8298a9e1246c26
RuslanUC <dev_ruslan_uc@protonmail.com>
2022-10-18 19:11:36 +0300

    update test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ async def test_with_rqdata_chromedriver():
     solver = AioHcaptcha("f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34", "https://discord.com/channels/@me",
                          {"chromedriver": "chromedriver"})
     resp = await solver.solve(
-        retry_count=3,
+        retry_count=5,
         custom_params={"rqdata": "xHJHshn3p71FcYoVCW5zA3m2CFw59JXBecFaR2l90z/NjjoYaXq2FBTi05LPnOX1v/MwStZg9DZKQA4f4ExkDjwlMaS3AKGIrcb2rUKsg8nDI9IaXEFDAhWqvuuCuaW3urxO2J1B/NEkfS938O58cqrE00aPILCQPUHVU1l/Ek8"}
     )
     assert resp is not None
